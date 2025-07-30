73857a1540719bb70ae69d217fcc535934712c5a
Timofey Gridin <timasgridin@gmail.com>
2025-07-30 17:49:52 +0300

    Создан градл проект с информацией обо мне

---
+++ src/main/java/ru/mentee/power/variables/VariableExample.java
@@ -0,0 +1,12 @@
+package ru.mentee.power.variables;
+
+public class VariableExample {
+    public static void main(String[] args) {
+        int myAge = 18;
+
+        String myName = "Tima";
+
+        System.out.println("My name is " + myName + " and I am " + myAge + " y.o.");
+
+    }
+}
